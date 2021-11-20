9eb645958c451352e6f40e5fd5fddf432a9a7726
yash62611 <94731429+yash62611@users.noreply.github.com>
2021-11-20 12:00:14 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ e package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Welcome home!");
+        System.out.println("Welcome home Hero!");
     }
 }
